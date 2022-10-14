02e48ff95c53b1c8972425527ad27f5f39a2b800
kkamauj <johnn2k@proton.me>
2022-10-14 13:29:11 -0700

    C program that prints a string without using printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Entry
+ *
+ * Return: 0 (If successful)
+ */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
