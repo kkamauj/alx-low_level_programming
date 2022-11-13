910450e52a0e2fabc77a83e4e5c7529644eff350
kkamauj <johnn2k@proton.me>
2022-11-12 18:03:14 -0800

    adds tasks 0-2

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: bytes
+ *
+ * Return: pointer allocated memory
+ * if malloc fails, status is equal to 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+	
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
